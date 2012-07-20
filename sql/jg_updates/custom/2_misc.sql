979cbadebcd454bf3234693f2f3701fae2adf610
Gary <fishermanpl@gmail.com>
2012-07-20 11:01:19 +0200

    Fix Priest: Shadowfiend.

--- sql/jg_updates/custom/2_misc.sql
+++ sql/jg_updates/custom/2_misc.sql
@@ -79,7 +79,14 @@ DELETE FROM spell_bonus_data WHERE entry = 33763;
 DELETE FROM spell_bonus_data WHERE entry = 48450;
 DELETE FROM spell_bonus_data WHERE entry = 48451;
 INSERT INTO spell_bonus_data (entry, direct_bonus, dot_bonus, ap_bonus, ap_dot_bonus, comment) VALUES
-(33778, 0, 0, 0, 0, 'Druid - Lifebloom's'),
+(33778, 0, 0, 0, 0, 'Druid - Lifebloom's bloom'),
 (33763, 0.516, 0.0952, 0, 0, 'Druid - Lifebloom Rank 1'),
 (48450, 0.516, 0.0952, 0, 0, 'Druid - Lifebloom Rank 2'), 
 (48451, 0.516, 0.0952, 0, 0, 'Druid - Lifebloom Rank 3');
+
+-- Priest: Shadowfiend, http://old.wowhead.com/spell=34433
+DELETE FROM `spell_proc_event` WHERE `entry` = 28305;
+INSERT INTO `spell_proc_event` VALUES (28305, 0, 0, 0, 0, 0, 0, 65536, 0, 0, 0);
+-- Move Shadowfiend's Mana Leech Aura from spellscript to creature addon.
+DELETE FROM `spell_script_names` WHERE `ScriptName`= 'spell_pri_shadowfiend';
+UPDATE `creature_template_addon` SET `auras`= '28305' WHERE `entry`=19668;
